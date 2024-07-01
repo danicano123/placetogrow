b0e017612c68f0f00fe5aff217a7cdd6ee7ffb30
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-30 22:43:33 -0500

    feat: all routes to use the microsite services

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\MicrositeController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Auth\Middleware\Authorize;
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
+    Route::get('/microsites/active', [MicrositeController::class, 'getActiveMicrosites']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
     Route::middleware(JwtMiddleware::class)->group(function () {
         Route::prefix('users')->group(function () {
             Route::get('', [UserController::class, 'index'])->middleware(Authorize::using('read users'));
@@ -20,7 +24,12 @@ Route::prefix('v1')->group(function () {
             Route::post('{userId}/assign-role', [UserController::class, 'assignRole'])->middleware(Authorize::using('edit users'));
             Route::post('{userId}/remove-role', [UserController::class, 'removeRole'])->middleware(Authorize::using('edit users'));
         });
+        Route::prefix('microsites')->group(function () {
+            Route::get('', [MicrositeController::class, 'index'])->middleware(Authorize::using('read microsites'));
+            Route::get('{id}', [MicrositeController::class, 'show'])->middleware(Authorize::using('read microsites'));
+            Route::post('', [MicrositeController::class, 'store'])->middleware(Authorize::using('create microsites'));
+            Route::patch('{id}', [MicrositeController::class, 'update'])->middleware(Authorize::using('edit microsites'));
+            Route::patch('{id}/is-active', [MicrositeController::class, 'toggleIsActive'])->middleware(Authorize::using('edit microsites'));
+        });
     });
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
 });
