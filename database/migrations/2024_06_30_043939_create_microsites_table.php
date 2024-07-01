51ab2b9d89999db3161fc291419162cac3a38618
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-30 22:44:39 -0500

    feat: microsites table migration

--- database/migrations/2024_06_30_043939_create_microsites_table.php
+++ database/migrations/2024_06_30_043939_create_microsites_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Constants\DocumentTypes;
+use App\Constants\MicrositeTypes;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,16 @@ return new class extends Migration
     {
         Schema::create('microsites', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('slug', 50)->unique();
+            $table->string('logo_url')->nullable();
+            $table->string('category', 50);
+            $table->enum('microsite_type', MicrositeTypes::values());
+            $table->string('currency_type', 10);
+            $table->integer('payment_expiration_time')->nullable();
+            $table->enum('document_type', DocumentTypes::values())->nullable();
+            $table->string('document', 50)->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
